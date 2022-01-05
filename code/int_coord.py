e2381321c87452a71be7b0c34d0013dee607902f
Leonardo J. Duarte <leo.j.duarte@hotmail.com.br>
2022-01-05 20:13:41 -0300

    update 0501

---
+++ code/int_coord.py
@@ -0,0 +1,182 @@
+import numpy as np
+
+
+def bond(geo, a, b):
+    """Calculates the bond stretch internal coordinate between a and b in the molecule
+
+    :param geo: molecule geometry in Cartesian Coordinates
+    :type geo: 2D array  
+    :param a: label of atom a
+    :type a: int
+    :param b: label of atom b
+    :type b: int]
+    :return: b_row
+    :rtype: 1D array 
+    """
+
+    v = (geo[a-1]-geo[b-1])/np.linalg.norm(geo[a-1]-geo[b-1])
+    b_row = np.zeros(3*len(geo))
+    b_row[3*a-3] = v[0]
+    b_row[3*a-2] = v[1]
+    b_row[3*a-1] = v[2]
+    b_row[3*b-3] = -v[0]
+    b_row[3*b-2] = -v[1]
+    b_row[3*b-1] = -v[2]
+
+    return b_row
+
+
+def angle(geo, a, b, c):
+    """Calculates the valence angle bend internal coordinate between atoms a-b-c
+
+    :param geo: molecular geometru in Cartesian Coordinates
+    :type geo: 2D array
+    :param a: label of atom a
+    :type a: int
+    :param b: label of atom b
+    :type b: int
+    :param c: label of atom c
+    :type c: int
+    :return: b_row
+    :rtype: 1D array
+    """
+
+    v1 = geo[a-1]-geo[b-1]
+    r1 = np.linalg.norm(v1)
+    v1 = v1/r1
+    v2 = geo[c-1]-geo[b-1]
+    r2 = np.linalg.norm(v2)
+    v2 = v2/r2
+    normal = np.cross(v1, v2)/np.linalg.norm(np.cross(v1, v2))
+    u1 = np.cross(v1, normal)
+    u1 = u1/np.linalg.norm(u1)
+    u1 = u1/r1
+    u2 = np.cross(normal, v2)
+    u2 = u2/np.linalg.norm(u2)
+    u2 = u2/r2
+
+    b_row = np.zeros(3*len(geo))
+    b_row[3*a-3] = u1[0]
+    b_row[3*a-2] = u1[1]
+    b_row[3*a-1] = u1[2]
+    b_row[3*c-3] = u2[0]
+    b_row[3*c-2] = u2[1]
+    b_row[3*c-1] = u2[2]
+    b_row[3*b-3] = -(u1[0]+u2[0])
+    b_row[3*b-2] = -(u1[1]+u2[1])
+    b_row[3*b-1] = -(u1[2]+u2[ 2])
+
+    return b_row
+
+
+def torsion(geo, a, b, c, d):
+    """calculates the torsion internal coordinate between n atoms a, atom b, atom c and m atoms d
+
+    :param geo: molecular geometry in Cartesian Coordinates
+    :type geo: 2D array
+    :param a: list of labels for the n a-type atoms 
+    :type a: 1D array
+    :param b: label of atom b
+    :type b: int
+    :param c: label of atom c
+    :type c: int
+    :param d: list of labels for the n a-type atoms 
+    :type d: 1D array
+    """
+
+    n = len(a)
+    m = len(d)
+    b_row = np.zeros(3*len(geo))
+    
+    bc = geo[c-1]-geo[b-1]
+    r_bc = np.linalg.norm(bc)
+    bc = bc/r_bc
+    cb = -bc
+    vb = [0, 0, 0]
+    vc = [0, 0, 0]
+
+    for a in a:
+        v1 = geo[a-1]-geo[b-1]
+        r1 = np.linalg.norm(v1)
+        v1 = v1/r1
+        ua = np.cross(v1, bc)/(r1*n*np.linalg.norm(np.cross(v1, bc))**2)
+        b_row[3*a-3] = ua[0]
+        b_row[3*a-2] = ua[1]
+        b_row[3*a-1] = ua[2]
+        vb = vb - (r_bc-r1*(np.dot(v1, bc)))*ua/r_bc
+        vc = vc - np.dot(cb, v1)*r1*ua/(r_bc)
+
+
+    for d in d:
+        v2 = geo[d-1]-geo[c-1]
+        r2 = np.linalg.norm(v2)
+        v2 = v2/r2
+        ud = np.cross(v2, cb)/(r2*m*np.linalg.norm(np.cross(v2, cb))**2)
+        b_row[3*d-3] = ud[0]
+        b_row[3*d-2] = ud[1]
+        b_row[3*d-1] = ud[2]
+        vb = vb - np.dot(bc,v2)*r2*ud/(r_bc)
+        vc = vc - (r_bc-r2*(np.dot(v2 , cb)))*ud/r_bc
+
+    b_row[3*b-3] = vb[0]
+    b_row[3*b-2] = vb[1]
+    b_row[3*b-1] = vb[2]
+    b_row[3*c-3] = vc[0]
+    b_row[3*c-2] = vc[1]
+    b_row[3*c-1] = vc[2]
+
+    return b_row
+
+def wag(geo, a, b, c, d):
+    """Calculates the out-of plane wag for the plane defined by atoms a, c and d.
+
+    :param geo: molecular geometry in Cartesian coordinates
+    :type geo: 2D array 
+    :param a: label of central atom a
+    :type a: int
+    :param b: label of atom b
+    :type b: int
+    :param c: label of atom c
+    :type c: int
+    :param d: label of atom d
+    :type d: int
+    """
+
+    b_row = np.zeros(3*len(geo))
+
+    vab = geo[a-1]-geo[b-1]
+    rab = np.linalg.norm(vab)
+    eab = vab/rab
+    vac = geo[a-1]-geo[c-1]
+    rac = np.linalg.norm(vac)
+    eac = vac/rac
+    vad = geo[a-1]-geo[d-1]
+    rad = np.linalg.norm(vad)
+    ead = vad/rad
+
+    normal = np.cross(eac, ead)/np.linalg.norm(np.cross(eac, ead))
+
+    ub = normal/rab
+    uc = (normal/rac) * (np.linalg.norm(np.cross(eab, ead))/ np.linalg.norm(np.cross(eac, ead)))
+    ud = (normal/rad) * (np.linalg.norm(np.cross(eab, eac))/ np.linalg.norm(np.cross(eac, ead)))
+    ua = -ub-uc-ud
+
+    b_row[3*a-3] = ua[0]
+    b_row[3*a-2] = ua[1]
+    b_row[3*a-1] = ua[2]
+    b_row[3*b-3] = ub[0]
+    b_row[3*b-2] = ub[1]
+    b_row[3*b-1] = ub[2]
+    b_row[3*c-3] = uc[0]
+    b_row[3*c-2] = uc[1]
+    b_row[3*c-1] = uc[2]
+    b_row[3*d-3] = ud[0]
+    b_row[3*d-2] = ud[1]
+    b_row[3*d-1] = ud[2]
+
+    return b_row
+
+def linear_angle(geo, a, b, c, deg=False):
+    
+
+    pass
---
+++ code/molecule.py
@@ -0,0 +1,42 @@
+"""Define the "Molecule" class.
+"""
+#import os
+class Molecule:
+    def __init__(self, atoms, positions):
+        self.atoms = atoms
+        self.positions = positions
+        #self.gaussian_path = gaussian_path
+        #self.aimall_path = aimall_path
+
+#    def __setattr__(self, __name: str, __value: Any) -> None:
+ #       pass
+
+
+ #   def opt(self, nproc, mem,  method, base, charge, mult):
+ #       f = open('molecule.gjf', 'w')
+ #       print('%%nproc = %i ' %(nproc), file=f)
+ #       print('%%mem = %i GB ' %(mem), file=f)
+ #       print('', file=f)
+ #       print('#opt %s %s density=current nosymm'%(method, base), file=f)
+ #       print('', file=f)
+ #       print('optimization', file=f)
+ #       print('', file=f)
+ #       print('%i %i'%(charge, mult), file=f)
+ #       for i in range(len(self.atoms)):
+  #          print('%s %.5f %.5f %.5f'%(self.atoms[i], self.positions[i][0], self.positions[i][1], self.positions[i][2]), file=f)
+  #      print('', file=f)
+  #      f.close()
+
+  #      os.system(self.gaussian_path + '  molecule.gjf')   
+   
+ #  def freq(nproc, mem, base, method, charge, mult):
+ #   def sp(nproc, mem, base, method, charge, mult):
+
+#    def first_derivative():
+
+#    def second_derivative():
+    
+
+
+
+
