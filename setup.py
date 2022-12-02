fee3f871262a83b82e4fa51edd8c3cacf63dc0eb
Leonardo J. Duarte <leo.j.duarte@hotmail.com.br>
2022-12-01 22:58:55 -0300

    version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="theovib",
-    version="0.0.3",
+    version="0.0.4",
     author="L. J. Duarte",
     author_email="leo.j.duarte49@gmail.com",
     description="Tools for molecular vibrations analysis",
